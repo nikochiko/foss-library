0fd9881d269bdbfac24919a1e19f0bc235de6118
Kaustubh Maske Patil <kaustubh299792458@gmail.com>
2021-09-09 11:23:06 +0530

    Add ondelete=SET NULL on foreign keys from transactions

--- foss_library/transactions/models.py
+++ foss_library/transactions/models.py
@@ -1,7 +1,5 @@
 from datetime import datetime
 
-from sqlalchemy import func
-
 from foss_library.database import CRUDMixin, db
 
 
@@ -9,15 +7,15 @@ class Transaction(db.Model, CRUDMixin):
     __tablename__ = "transactions"
 
     id = db.Column(db.Integer, primary_key=True)
-    book_id = db.Column(db.ForeignKey("books.id"), nullable=False)
+    book_id = db.Column(db.ForeignKey("books.id", ondelete="SET NULL"), nullable=False)
     book = db.relationship("Book", backref="transactions")
-    member_id = db.Column(db.ForeignKey("members.id"), nullable=False)
+    member_id = db.Column(db.ForeignKey("members.id", ondelete="SET NULL"), nullable=False)
     member = db.relationship("Member", backref="transactions")
-    borrowed_at = db.Column(db.DateTime, default=func.now())
+    borrowed_at = db.Column(db.DateTime, default=db.func.now())
     returned_at = db.Column(db.DateTime, nullable=True, index=True)
 
-    created_at = db.Column(db.DateTime, default=func.now())
-    updated_at = db.Column(db.DateTime, default=func.now(), onupdate=func.now())
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
 
     @property
     def is_completed(self):
