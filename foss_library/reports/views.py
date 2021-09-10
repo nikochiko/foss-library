56cb909fb61009c985111886dff342f7dbbcb6b3
Kaustubh Maske Patil <kaustubh299792458@gmail.com>
2021-09-10 12:25:38 +0530

    Add highest paying customers report

--- foss_library/reports/views.py
+++ foss_library/reports/views.py
@@ -4,6 +4,7 @@ from flask import Blueprint, flash, render_template, request
 
 from foss_library.database import db
 from foss_library.books.models import Book
+from foss_library.members.models import Member
 from foss_library.transactions.models import Transaction
 
 
@@ -14,12 +15,17 @@ blueprint = Blueprint(
 
 def get_most_popular_books_query():
     most_popular_books_query = (
-        Book.query.join(Transaction, Book.id == Transaction.book_id)
+        Book.query.join(Transaction, Book.id == Transaction.book_id, full=True)
         .add_columns(db.text(f"count({Transaction.__tablename__}.id) as popularity"))
         .order_by(db.text("popularity desc"))
         .group_by(Book.id)
     )
     return most_popular_books_query
+
+
+def get_highest_paying_customers_query():
+    highest_paying_customers_query = Member.query.order_by(Member.dues_paid.desc())
+    return highest_paying_customers_query
 
 
 @blueprint.route("/")
@@ -58,4 +64,28 @@ def most_popular_books_report():
 
 @blueprint.route("/highest-paying-customers")
 def highest_paying_customers_report():
-    return "hello"
+    on_each_page = 20
+
+    # get page from request args, e.g. /books?page=1
+    page = request.args.get("page", 1)
+    page = int(page)
+
+    # set limit and offset for SQL query
+    limit = on_each_page
+    offset = (page - 1) * on_each_page
+
+    query = get_highest_paying_customers_query()
+    highest_paying_customers = query.limit(limit).offset(offset).all()
+
+    total_pages = ceil(query.count() / on_each_page)
+
+    if page > total_pages:
+        flash(f"Page {page} is out of range", "warning")
+
+    return render_template(
+        "reports/highest_paying_customers_report.html",
+        highest_paying_customers=highest_paying_customers,
+        current_page=page,
+        total_pages=total_pages,
+        offset=offset,
+    )
